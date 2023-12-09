a16f00b963d0c7a2ac5262dfa331ad74b6ee4330
NICOLA-200 <fatepepe66@gmail.com>
2023-12-09 10:16:02 +0200

    database addedd

---
+++ utils/database.js
@@ -0,0 +1,15 @@
+import  {  Schema ,  model,  models }  from 'mongoose';
+
+
+const userSchema = new Schema({
+     email: {
+          type: String,
+          unique: [true ,'Email already  exists!'],
+          required: [true, "Email is required"]
+     },
+     {
+          username: {
+               type : String
+          }
+     }
+}) 
