9298264847189d5467e74a062d2f11c0706d8aef
NICOLA-200 <fatepepe66@gmail.com>
2023-12-09 10:21:03 +0200

    nav   provider added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -32,3 +32,43 @@ const Nav = () => {
         />
         <p className='logo_text'>Promptopia</p>
       </Link>
+
+          {/* Desktop Navigation */}
+          <div className='sm:flex hidden'>
+        {session?.user ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
