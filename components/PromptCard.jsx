c97c06c685f2d7dd944282b6974246c36879e38b
NICOLA-200 <fatepepe66@gmail.com>
2023-12-10 19:19:58 +0200

    prompt  functionality added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -25,3 +25,29 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(false), 3000);
   };
+
+  return (
+     <div className='prompt_card'>
+       <div className='flex justify-between items-start gap-5'>
+         <div
+           className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+           onClick={handleProfileClick}
+         >
+           <Image
+             src={post.creator.image}
+             alt='user_image'
+             width={40}
+             height={40}
+             className='rounded-full object-contain'
+           />
+ 
+           <div className='flex flex-col'>
+             <h3 className='font-satoshi font-semibold text-gray-900'>
+               {post.creator.username}
+             </h3>
+             <p className='font-inter text-sm text-gray-500'>
+               {post.creator.email}
+             </p>
+           </div>
+         </div>
+ 
