4b485be1e882d87f8cdb86ea74c5d053be66678a
NICOLA-200 <fatepepe66@gmail.com>
2023-12-10 19:20:49 +0200

    prompt card added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -78,4 +78,18 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             className='font-inter text-sm green_gradient cursor-pointer'
             onClick={handleEdit}
           >
-    
+       Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PromptCard;
