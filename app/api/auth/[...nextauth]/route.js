e4fdeca67dec20287a88618532f364b9832ada30
NICOLA-200 <fatepepe66@gmail.com>
2023-12-12 07:23:26 +0200

    auth started

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
