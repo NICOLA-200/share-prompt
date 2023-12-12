8bead4cf68fea0d135d285d551309a5d51b8b381
NICOLA-200 <fatepepe66@gmail.com>
2023-12-12 07:23:40 +0200

    auth ended

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -19,3 +19,29 @@ const handler = NextAuth({
 
       return session;
     },
+    async signIn({ account, profile, user, credentials }) {
+     try {
+       await connectToDB();
+
+       // check if user already exists
+       const userExists = await User.findOne({ email: profile.email });
+
+       // if not, create a new document and save user in MongoDB
+       if (!userExists) {
+         await User.create({
+           email: profile.email,
+           username: profile.name.replace(" ", "").toLowerCase(),
+           image: profile.picture,
+         });
+       }
+
+       return true
+     } catch (error) {
+       console.log("Error checking if user exists: ", error.message);
+       return false
+     }
+   },
+ }
+})
+
+export { handler as GET, handler as POST }
