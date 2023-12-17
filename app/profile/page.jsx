da2d84f684c8b8f02a5b2c0b232c84a2223433e8
NICOLA-200 <fatepepe66@gmail.com>
2023-12-17 14:15:42 +0200

    profile finished

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,3 +26,36 @@ const MyProfile = () => {
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
+  const handleDelete = async (post) => {
+     const hasConfirmed = confirm(
+       "Are you sure you want to delete this prompt?"
+     );
+ 
+     if (hasConfirmed) {
+       try {
+         await fetch(`/api/prompt/${post._id.toString()}`, {
+           method: "DELETE",
+         });
+ 
+         const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+ 
+         setMyPosts(filteredPosts);
+       } catch (error) {
+         console.log(error);
+       }
+     }
+   };
+ 
+   return (
+     <Profile
+       name='My'
+       desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+       data={myPosts}
+       handleEdit={handleEdit}
+       handleDelete={handleDelete}
+     />
+   );
+ };
+ 
+ export default MyProfile;
+ 
