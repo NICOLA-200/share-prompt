0d9e7ec1b6a5bb2a3dd8dc47e92ba388e0852468
NICOLA-200 <fatepepe66@gmail.com>
2023-12-17 14:15:19 +0200

    profile started

---
+++ app/profile/page.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
